1b500875d262714f0b25fba70231a2db9af77bf2
contatodct <66734743+contatodct@users.noreply.github.com>
2023-04-19 13:11:58 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 clear
-echo
-echo
-echo
-echo "======================= HOMETI VOIP SYSTEM FOR LINUX =======================";
-echo "=                                                                          =";
-echo "=                                                                          =";
-echo "=======================        By: Daniel Som        =======================";
-echo
-echo
-echo
+printf "\n"
 
-sleep 3
+  printf "${GREEN}";
+  printf "===================================================\n";
+  printf "##                                               ##\n";
+  printf "##      ███████         █████████   █████████    ##\n";
+  printf "##      ███    ██       ███            ███       ##\n";
+  printf "##      ███    ███      ███            ███       ##\n";
+  printf "##      ███    ███      ███            ███       ##\n";
+  printf "##      ███    ██       ███            ███       ##\n";
+  printf "##      ███████         █████████      ███       ##\n";
+  printf "##                                               ##\n";
+  printf "##    ESSE MATERIAL FAZ PARTE DA DCT SISTEMAS    ##\n";
+  printf "##                                               ##\n";
+  printf "##         PIRATEAR ESSA SOLUÇÃO É CRIME.        ##\n";
+  printf "##                                               ##\n";
+  printf "##    © DCT-SISTEMAS  -  www.dctsistemas.com     ##\n";
+  printf "##                                               ##\n";
+  printf "===================================================\n";
+  printf "\n"; 
+
+sleep 5
 
 
 if [[ -f /var/www/html/mbilling/index.php ]]; then
@@ -660,84 +670,6 @@ mysql -uroot -pP5JkUEYSms4M4Igw -e "update mysql.user set plugin='' where User='
 fi;
 mysql mbilling -u root -pP5JkUEYSms4M4Igw  < /var/www/html/mbilling/script/database.sql
 
-# CRIANDO REPOSITORIO PARA WEBMIN
-cat <<[-]EOF > /etc/yum.repos.d/webmin.repo
-[Webmin]
-name=Webmin Distribution Neutral
-#baseurl=http://download.webmin.com/download/yum
-mirrorlist=http://download.webmin.com/download/yum/mirrorlist
-enabled=1
-[-]EOF
-
-
-# INSTALANDO WEBMIN
-sudo su - root << EOF
-rpm --import http://www.webmin.com/jcameron-key.asc
-yum check-update
-yum install webmin -y
-EOF
-
-# INICIANDO WEBMIN
-sudo su - root << EOF
-systemctl enable webmin.service
-/bin/systemctl start webmin.service
-EOF
-
-# CONFIGURANDO WEBMIN
-cat <<[-]EOF > /etc/webmin/miniserv.conf
-port=18181
-root=/usr/libexec/webmin
-mimetypes=/usr/libexec/webmin/mime.types
-addtype_cgi=internal/cgi
-realm=Webmin Server
-logfile=/var/webmin/miniserv.log
-errorlog=/var/webmin/miniserv.error
-pidfile=/var/webmin/miniserv.pid
-logtime=168
-ssl=0
-no_ssl2=1
-no_ssl3=1
-no_tls1=1
-no_tls1_1=1
-ssl_honorcipherorder=1
-no_sslcompression=1
-env_WEBMIN_CONFIG=/etc/webmin
-env_WEBMIN_VAR=/var/webmin
-atboot=1
-logout=/etc/webmin/logout-flag
-listen=10000
-denyfile=\.pl$
-log=1
-blockhost_failures=5
-blockhost_time=60
-syslog=1
-ipv6=1
-session=1
-premodules=WebminCore
-server=MiniServ/2.021
-userfile=/etc/webmin/miniserv.users
-keyfile=/etc/webmin/miniserv.pem
-passwd_file=/etc/shadow
-passwd_uindex=0
-passwd_pindex=1
-passwd_cindex=2
-passwd_mindex=4
-passwd_mode=0
-preroot=authentic-theme
-passdelay=1
-login_script=/etc/webmin/login.pl
-logout_script=/etc/webmin/logout.pl
-cipher_list_def=1
-failed_script=/etc/webmin/failed.pl
-error_handler_403=403.cgi
-error_handler_404=404.cgi
-error_handler_401=401.cgi
-[-]EOF
-
-# REINICIANDO WEBMIN
-sudo su - root << EOF
-/bin/systemctl restart webmin.service
-EOF
 
 
 
